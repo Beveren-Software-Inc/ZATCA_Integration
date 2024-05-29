398a8dec76a3128708522249b87fb0542e1bc60f
realespee <wanyamasp@gmail.com>
2024-05-29 23:09:44 +0300

    Resolve Sales Invoice Not Saved Error

--- zatca_integration/customization/sales_invoice/sales_invoice.js
+++ zatca_integration/customization/sales_invoice/sales_invoice.js
@@ -5,13 +5,10 @@ frappe.ui.form.on('Sales Invoice', {
     },
     refresh: frm => {
         frm.trigger('set_custom_payment_method')
-        // Trigger update when item is added or removed
-        // frm.fields_dict['items'].grid.get_field('delivery_note').get_query = function(doc) {
-        //     frm.trigger('set_delivery_date')
-        // }
+        frm.trigger('set_delivery_date')
     },
     set_custom_payment_method: frm => {
-        if(frm.doc.customer){
+        if(frm.doc.customer && !frm.doc.custom_payment_means){
             frappe.call({
                 method: "zatca_integration.customization.sales_invoice.sales_invoice.update_payment_method",
                 args: {
@@ -28,26 +25,28 @@ frappe.ui.form.on('Sales Invoice', {
         }
     },
     set_delivery_date: frm => {
-        // check if items array has some items
-        const items = frm.doc.items || [];
-        const deliveryNotes = [...new Set(items.map(item => item.delivery_note).filter(Boolean))];
+        if(!frm.doc.custom_delivery_date){
+            // check if items array has some items
+            const items = frm.doc.items || [];
+            const deliveryNotes = [...new Set(items.map(item => item.delivery_note).filter(Boolean))];
 
-        console.log(deliveryNotes);
+            console.log(deliveryNotes);
 
-        if (deliveryNotes.length > 0) {
-            // Fetch the delivery date from the of the notes
-            frappe.call({
-                method: "zatca_integration.customization.sales_invoice.sales_invoice.update_delivery_date",
-                args: {
-                   delivery_note: deliveryNotes[0]
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        // Set the delivery date to the invoice
-                        frm.set_value('custom_delivery_date', r.message);
+            if (deliveryNotes.length > 0) {
+                // Fetch the delivery date from the of the notes
+                frappe.call({
+                    method: "zatca_integration.customization.sales_invoice.sales_invoice.update_delivery_date",
+                    args: {
+                    delivery_note: deliveryNotes[0]
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            // Set the delivery date to the invoice
+                            frm.set_value('custom_delivery_date', r.message);
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     }
 });
