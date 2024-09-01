694fe868e29f89604c76655117a70850f09d50f3
Shakir Mukkath <131162756+shakirpm@users.noreply.github.com>
2024-09-01 15:17:33 -0400

    Removing ZATCA Settings Page

--- zatca_integration/saudi_arabia_electronic_invoicing/doctype/zatca_settings/test_zatca_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Shakir PM and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestZatcaSettings(FrappeTestCase):
-	pass
