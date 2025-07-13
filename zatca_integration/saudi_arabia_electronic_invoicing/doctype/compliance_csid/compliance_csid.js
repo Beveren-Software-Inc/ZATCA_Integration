57d984f59021a7baa81919b31a721aed27563566
maniamartial <martialmania19@gmail.com>
2025-07-13 10:48:53 +0300

    chore: Disable button  of compliance csic, zatca settings, and production csid once generated cert

--- zatca_integration/saudi_arabia_electronic_invoicing/doctype/compliance_csid/compliance_csid.js
+++ zatca_integration/saudi_arabia_electronic_invoicing/doctype/compliance_csid/compliance_csid.js
@@ -1,25 +1,83 @@
-// Copyright (c) 2024, Shakir PM and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2024, Shakir PM and contributors
+// // For license information, please see license.txt
+
+// frappe.ui.form.on("Compliance CSID", {
+// 	refresh: frm => {
+//         if(!frm.is_new()) {
+//             frm.trigger("genereate_zatca_compliance_csid");
+//             frm.trigger("validate_zatca_compliance_csid");
+//         }
+//     },
+//     genereate_zatca_compliance_csid: frm => {
+//             frm.add_custom_button(__('Generate Compliance CSID'), function() {
+//             // Show a progress message
+//             frappe.show_progress(__('Generating Compliance CSID...'));
+//             // Call the server side function
+//             frappe.call({
+//                 method: "genereate_zatca_compliance_csid",
+//                 doc: frm.doc,
+//                 callback: function(r) {
+//                     frappe.hide_progress();
+//                     if(!r.exc) {
+//                         frappe.show_alert({message:__('Compliance CSID Generated Successfully!'), indicator:'green'});
+//                         frm.reload_doc();
+//                     } else {
+//                         frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
+//                     }
+//                 },
+//                 error: function(r) {
+//                     frappe.hide_progress();
+//                     frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
+//                 }
+//             });
+//         });
+//     },
+//     validate_zatca_compliance_csid: frm => {
+//             frm.add_custom_button(__('Validate Compliance CSID'), function() {
+//             // Show a progress message            
+//             frappe.show_progress(__('Validating Compliance CSID...'));
+//             // Call the server side function
+//             frappe.call({
+//                 method: "validate_zatca_compliance_csid",
+//                 doc: frm.doc,
+//                 callback: function(r) {
+//                     frappe.hide_progress();
+//                     if(!r.exc) {
+//                         frappe.show_alert({message:__('Compliance CSID Validated Successfully!'), indicator:'green'});
+//                         frm.reload_doc();
+//                     } else {
+//                         frappe.show_alert({ message: __('Failed to Validate Compliance CSID'), indicator: 'red' });
+//                     }
+//                 },
+//                 error: function(r) {
+//                     frappe.hide_progress();
+//                     frappe.show_alert({ message: __('Failed to Validate Compliance CSID'), indicator: 'red' });
+//                 }
+//             });
+//         })
+//     }
+// });
 
 frappe.ui.form.on("Compliance CSID", {
-	refresh: frm => {
-        if(!frm.is_new()) {
-            frm.trigger("genereate_zatca_compliance_csid");
-            frm.trigger("validate_zatca_compliance_csid");
+    refresh: frm => {
+        if (!frm.is_new()) {
+            if (!are_all_flags_true(frm)) {
+                frm.trigger("genereate_zatca_compliance_csid");
+                frm.trigger("validate_zatca_compliance_csid");
+            }
         }
     },
+
     genereate_zatca_compliance_csid: frm => {
-            frm.add_custom_button(__('Generate Compliance CSID'), function() {
-            // Show a progress message
+        frm.add_custom_button(__('Generate Compliance CSID'), function() {
             frappe.show_progress(__('Generating Compliance CSID...'));
-            // Call the server side function
             frappe.call({
                 method: "genereate_zatca_compliance_csid",
                 doc: frm.doc,
                 callback: function(r) {
                     frappe.hide_progress();
-                    if(!r.exc) {
-                        frappe.show_alert({message:__('Compliance CSID Generated Successfully!'), indicator:'green'});
+                    if (!r.exc) {
+                        frappe.show_alert({ message: __('Compliance CSID Generated Successfully!'), indicator: 'green' });
                         frm.reload_doc();
                     } else {
                         frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
@@ -32,18 +90,17 @@ frappe.ui.form.on("Compliance CSID", {
             });
         });
     },
+
     validate_zatca_compliance_csid: frm => {
-            frm.add_custom_button(__('Validate Compliance CSID'), function() {
-            // Show a progress message            
+        frm.add_custom_button(__('Validate Compliance CSID'), function() {
             frappe.show_progress(__('Validating Compliance CSID...'));
-            // Call the server side function
             frappe.call({
                 method: "validate_zatca_compliance_csid",
                 doc: frm.doc,
                 callback: function(r) {
                     frappe.hide_progress();
-                    if(!r.exc) {
-                        frappe.show_alert({message:__('Compliance CSID Validated Successfully!'), indicator:'green'});
+                    if (!r.exc) {
+                        frappe.show_alert({ message: __('Compliance CSID Validated Successfully!'), indicator: 'green' });
                         frm.reload_doc();
                     } else {
                         frappe.show_alert({ message: __('Failed to Validate Compliance CSID'), indicator: 'red' });
@@ -54,6 +111,17 @@ frappe.ui.form.on("Compliance CSID", {
                     frappe.show_alert({ message: __('Failed to Validate Compliance CSID'), indicator: 'red' });
                 }
             });
-        })
+        });
     }
 });
+
+// Helper function to check all fields
+function are_all_flags_true(frm) {
+    return frm.doc.standard_invoice &&
+           frm.doc.standard_debit_note &&
+           frm.doc.standard_credit_note &&
+           frm.doc.simplified_invoice &&
+           frm.doc.simplified_debit_note &&
+           frm.doc.simplified_credit_note;
+}
+
