436a57b5da75fabb8d5e4a090176e4ac8f7a5a06
maniamartial <martialmania19@gmail.com>
2025-07-22 11:06:21 +0300

    feat: Do away with test button of generating invoices

--- zatca_integration/saudi_arabia_electronic_invoicing/doctype/compliance_csid/compliance_csid.js
+++ zatca_integration/saudi_arabia_electronic_invoicing/doctype/compliance_csid/compliance_csid.js
@@ -6,13 +6,13 @@ frappe.ui.form.on("Compliance CSID", {
                 frm.trigger("genereate_zatca_compliance_csid");
 
             }
-            // if (!are_all_flags_true(frm) && frm.doc.binary_security_token) {
+            if (!are_all_flags_true(frm) && frm.doc.binary_security_token) {
                 frm.trigger("validate_zatca_compliance_csid");
                 
-            // }
+            }
         }
         make_fields_read_only(frm);
-        create_test_zatca_compliance_csid(frm);
+        // create_test_zatca_compliance_csid(frm);
     },
 
     genereate_zatca_compliance_csid: frm => {
@@ -84,27 +84,27 @@ function make_fields_read_only(frm) {
     }
 }
 
-function create_test_zatca_compliance_csid(frm) {
-    frm.add_custom_button(__('Generate Test Data'), function () {
-        frappe.show_progress(__('Generating Compliance CSID...'));
-        frappe.call({
-            method: "zatca_integration.saudi_arabia_electronic_invoicing.data.test_data.create_return_invoice",
-            args: {
-                csr: frm.doc.csr_settings
-            },
-            callback: function (r) {
-                frappe.hide_progress();
-                if (!r.exc) {
-                    frappe.show_alert({ message: __('Compliance CSID Generated Successfully!'), indicator: 'green' });
-                    frm.reload_doc();
-                } else {
-                    frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
-                }
-            },
-            error: function (r) {
-                frappe.hide_progress();
-                frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
-            }
-        });
-    });
-}
+// function create_test_zatca_compliance_csid(frm) {
+//     frm.add_custom_button(__('Generate Test Data'), function () {
+//         frappe.show_progress(__('Generating Compliance CSID...'));
+//         frappe.call({
+//             method: "zatca_integration.saudi_arabia_electronic_invoicing.data.test_data.create_return_invoice",
+//             args: {
+//                 csr: frm.doc.csr_settings
+//             },
+//             callback: function (r) {
+//                 frappe.hide_progress();
+//                 if (!r.exc) {
+//                     frappe.show_alert({ message: __('Compliance CSID Generated Successfully!'), indicator: 'green' });
+//                     frm.reload_doc();
+//                 } else {
+//                     frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
+//                 }
+//             },
+//             error: function (r) {
+//                 frappe.hide_progress();
+//                 frappe.show_alert({ message: __('Failed to Generate Compliance CSID'), indicator: 'red' });
+//             }
+//         });
+//     });
+// }
