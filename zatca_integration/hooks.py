24b105a2e2e3ff37b8ee26c67db0d3072911c448
maniamartial <martialmania19@gmail.com>
2025-10-22 11:57:03 +0300

    feat: Add fields on the company

--- zatca_integration/hooks.py
+++ zatca_integration/hooks.py
@@ -81,6 +81,9 @@ fixtures = [
                     "Company-custom_csr_settings",
                     "Company-custom_production_csid",
                     "Company-custom_e_invoice_settings",
+                    "Company-custom_column_break_t28uu",
+                    "Company-custom_enable_pdf3a",
+                    "Company-custom_convertapi_token",
                     # Sales Invoice
                     "Sales Invoice-custom_compliance",
                     "Sales Invoice-custom_cn_ref",
